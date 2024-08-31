b4154972e3f004280f5c91de2a5798a180ae9f3b
NAVEEN15020189 <ramadevi15020@gmail.com>
2024-08-31 21:54:06 +0530

    second

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 let mongoose=  require('mongoose');
-let url=" mongodb+srv://ramadevi15020:PbHLAAxhdhCw7RtF@naveen.qzklo.mongodb.net/?retryWrites=true&w=majority&appName=naveen";
+let url="mongodb+srv://ramadevi15020:PbHLAAxhdhCw7RtF@naveen.qzklo.mongodb.net/?retryWrites=true&w=majority&appName=naveen";
 mongoose.connect(url)
